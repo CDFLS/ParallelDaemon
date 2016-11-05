1d9021db1567490ee25e9dc6868b6f56893a80e5
t123yh <t123yh@outlook.com>
2016-11-05 21:52:28 +0800

    添加项目文件。

---
+++ src/ParallelDaemon/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParallelDaemon
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
